a9ff25e2b69abd01c9d0a64b844f9b5e5b269a8b
zeroylalus <armopc8@gmail.com>
2023-05-11 17:38:45 -0600

    commit

--- granadosmx/settings.py
+++ granadosmx/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'productos',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    #por que no lo encontraba en app sales/static/sales/home.js
+    BASE_DIR / 'productos' / 'templates',
+    #BASE_DIR / 'reports' / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
